2c799bd2d0b94322610aa0f7cb4ddd9f0c971476
Gábor Csárdi <csardi.gabor@gmail.com>
2016-08-30 15:14:47 +0100

    Brush up Mason template

--- R/package.R
+++ R/package.R
@@ -1,7 +1,8 @@
 
 #' Connect to 'R-hub', from 'R'
 #'
-#' Run 'R CMD check' on any of the 'R-hub' architectures, from the command line.
+#' Run 'R CMD check' on any of the 'R-hub' architectures, from the
+#' command line.
 #'
 #' @docType package
 #' @name rhub
