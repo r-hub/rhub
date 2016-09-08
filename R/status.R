88df658a0c924c44e1df5b53dc966799c70daf30
Gábor Csárdi <csardi.gabor@gmail.com>
2016-09-08 21:52:30 +0100

    Print status line by line

--- R/status.R
+++ R/status.R
@@ -7,12 +7,42 @@ my_curl_stream <- function(url, callback, bufsize = 80) {
     open(con, "rb")
     on.exit(close(con))
   }
-  while (length(buf <- readBin(con, raw(), bufsize))) callback(buf)
+  while (length(buf <- readBin(con, raw(), bufsize))) {
+    callback(buf)
+  }
+}
+
+byline <- function(fun) {
+  buffer <- raw(0)
+  function(r) {
+
+    ## Append new chunk to our buffer
+    r <- c(buffer, r)
+    buffer <- raw(0)
+
+    ## Search for the last newline, if any
+    nl <- tail(which(r == charToRaw('\n')), 1)
+    if (length(nl) == 0) {
+      buffer <<- r
+      return()
+
+    } else if (nl != length(r)) {
+      buffer <<- r[(nl + 1):length(r)]
+      r <- r[1:nl]
+    }
+
+    ## Time to convert to string, split into lines, and serve it
+    str <- rawToChar(r)
+    lines <- strsplit(str, "\n")[[1]]
+    for (l in lines) fun(l)
+  }
 }
 
 #' @export
 
 check_status <- function(id, interactive = interactive()) {
-  my_curl_stream(id$`log-url`, function(x) cat(rawToChar(x)))
-  invisible()
+  if (interactive) {
+    my_curl_stream(id$`log-url`, byline(function(x) cat(x, "\n", sep = "")))
+  }
+  id
 }
