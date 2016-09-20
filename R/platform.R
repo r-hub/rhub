290f6a821d9cfb7fa48eab6396ddc8d34c2998e5
Gábor Csárdi <csardi.gabor@gmail.com>
2016-09-20 22:50:57 +0100

    Pretty print platforms, closes #11

--- R/platform.R
+++ R/platform.R
@@ -1,8 +1,20 @@
 
 #' @export
 #' @importFrom jsonlite fromJSON
+#' @importFrom crayon green cyan
 
 platforms <- function() {
   json <- query("GET PLATFORMS", as = "text")
-  fromJSON(json, simplifyDataFrame = TRUE)
+  pls <- fromJSON(json, simplifyDataFrame = TRUE)
+  pls <- pls[order(pls$name), , drop = FALSE]
+  class(pls) <- c("rhub_platforms", pls)
+  pls
+}
+
+#' @export
+
+print.rhub_platforms <- function(x, ...) {
+  res <- paste(cyan(x$name), green(x$description), sep = ":\n  ")
+  cat(res, sep = "\n")
+  invisible(x)
 }
