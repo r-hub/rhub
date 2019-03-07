319fd197da3eea8ff1e9aaeee20d9c946799f967
Maëlle Salmon <maelle.salmon@yahoo.se>
2019-03-07 12:33:04 +0100

    Add method to retrieve URLs including artifacts URL (#213)
    Add method to retrieve URLs including artifacts URL

--- R/check-class.R
+++ R/check-class.R
@@ -34,7 +34,13 @@
 #' `ch$print()` prints the status of the check(s) to the screen.
 #'
 #' `ch$browse()` opens a tab or window in the default web browser, that points
-#' to the detailed logs of the check.
+#' to the detailed logs of the check(s).
+#' 
+#' `ch$urls()` return a table with URL to the html log, text log and artifacts
+#' of the check(s).
+#' 
+#' For both `ch$browse()` and `ch$urls()`, not that the logs and artifacts 
+#' are not kept forever, they are accessible for a few days after submission.
 #'
 #' `ch$livelog()` shows the live log of the check. The live log can be
 #' interrupted using the usual interruption keyboard shortcut, usually
@@ -73,6 +79,9 @@ rhub_check <- R6Class(
     
     browse = function(which = NULL)
       self$web(which),
+    
+    urls = function(which = NULL)
+      check_urls(self, private, which),
 
     livelog = function(which = 1)
       check_livelog(self, private, which)
@@ -102,6 +111,28 @@ check_update <- function(self, private) {
 
 check_web <- function(self, private, which) {
 
+  ids <- select_ids(which = which, self = self, 
+                    private = private)
+
+  urls <- paste0(sub("/api$", "/status/", baseurl()), ids)
+  
+  lapply(urls, browseURL)
+  invisible(self)
+}
+
+check_urls <- function(self, private, which) {
+  
+  ids <- select_ids(which = which, self = self, 
+                    private = private)
+  
+  data.frame(html = paste0(sub("/api$", "/status/", baseurl()), ids),
+             text = paste0(sub("/api$", "/status/original/", baseurl()), 
+                           ids),
+             artifacts = paste0("https://artifacts.r-hub.io/", ids),
+             stringsAsFactors = FALSE)
+}
+
+select_ids <- function(which, self, private){
   ids <- if (is.null(which)) {
     private$ids_
   } else if (is.numeric(which)) {
@@ -109,10 +140,9 @@ check_web <- function(self, private, which) {
   } else if (is.character(which)) {
     intersect(private$ids_, which)
   } else {
-    stop("Unknown check selected")
+    stop("Unknown check selected",
+         call. = FALSE)
   }
-
-  urls <- paste0(sub("/api$", "/status/", baseurl()), ids)
-  lapply(urls, browseURL)
-  invisible(self)
+  
+  return(ids)
 }
