8d34b691491f4d39696d5bbfd6616e6e9807663b
Maëlle Salmon <maelle.salmon@yahoo.se>
2020-02-04 14:50:43 +0100

    workaround?

--- tests/testthat/helper-rhub.R
+++ tests/testthat/helper-rhub.R
@@ -1,7 +1,20 @@
+# comment the commands below before recording test cassettes
+mockery::stub(where = list_validated_emails2,
+              what = "email_file", 
+              how = file.path(testthat::test_path(), "validated_emails.csv"))
+mockery::stub(where = email_get_token,
+              what = "email_file", 
+              how = file.path(testthat::test_path(), "validated_emails.csv"))
+mockery::stub(where = email_add_token,
+              what = "email_file", 
+              how = file.path(testthat::test_path(), "validated_emails.csv"))
+
 library("vcr")
 invisible(vcr::vcr_configure(
   dir = "../fixtures",
-  filter_sensitive_data = list("<<<my_rhub_token>>>" = rhub:::email_get_token(rhub:::list_validated_emails()[1]),
-                               "<<<my_email>>>" = rhub:::list_validated_emails()[[1]])
+  filter_sensitive_data = list("<<<my_rhub_token>>>" = email_get_token(list_validated_emails()[1]),
+                               "<<<my_email>>>" = list_validated_emails()[[1]])
 ))
 vcr::check_cassette_names()
+
+
